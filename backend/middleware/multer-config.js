1ee2192c20990df27c056bac24ee557ba9b08c96
Julien <julien.tissidre@gmail.com>
2023-09-13 21:54:25 +0200

    fix bug modifyBook sans image

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -10,37 +10,42 @@ function generateFileName(originalName) {
   return `${cleanedName}_${Date.now()}`;
 }
 
-module.exports = (req, res, next) => {
-  upload(req, res, async (err) => {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
+module.exports = (operation) => {
+  return (req, res, next) => {
+      upload(req, res, async (err) => {
+          if (err) {
+              return res.status(400).json({ error: err.message });
+          }
 
-    if (!req.file) {
-      return res.status(400).json({ error: 'Aucune image n\'a été envoyée' });
-    }
+          if (!req.file) {
+              if (operation === 'create') {
+                  return res.status(400).json({ error: 'Aucune image n\'a été envoyée' });
+              }
+              return next();
+          }
 
-    // Redimensionnement de l'image et enregistrement dans le dossier 'images'
-    try {
-      const resizedImageBuffer = await sharp(req.file.buffer)
-        .resize(({
-          width: 375,
-          height: 568,
-          fit: 'inside' 
-        }))
-        .webp({ quality: 80 }) 
-        .toBuffer()
+          // Redimensionnement de l'image et enregistrement dans le dossier 'images'
+          try {
+              const resizedImageBuffer = await sharp(req.file.buffer)
+                  .resize(({
+                      width: 375,
+                      height: 568,
+                      fit: 'inside' 
+                  }))
+                  .webp({ quality: 80 }) 
+                  .toBuffer()
 
-      const originalFileName = req.file.originalname;
-      const imageName = generateFileName(originalFileName) + '.webp'; 
-      const imagePath = 'images/' + imageName;
+              const originalFileName = req.file.originalname;
+              const imageName = generateFileName(originalFileName) + '.webp'; 
+              const imagePath = 'images/' + imageName;
 
-      require('fs').writeFileSync(imagePath, resizedImageBuffer);
+              require('fs').writeFileSync(imagePath, resizedImageBuffer);
 
-      req.imagePath = imagePath; 
-      next();
-    } catch (error) {
-      return res.status(500).json({ error: 'Erreur lors du redimensionnement de l\'image' });
-    }
-  });
+              req.imagePath = imagePath; 
+              next();
+          } catch (error) {
+              return res.status(500).json({ error: 'Erreur lors du redimensionnement de l\'image' });
+          }
+      });
+  }
 };
